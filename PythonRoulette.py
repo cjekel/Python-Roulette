c82240be5a1ab21afa4918e8652f6e26fc07f2d1
cjekel <cjekel@gmail.com>
2016-07-11 20:06:54 +0200

    add 'row' betting

--- PythonRoulette.py
+++ PythonRoulette.py
@@ -32,8 +32,37 @@ class roulette:
             self.colorHistory = []
         else:
             self.colorHistory = colorHistory
-   
-    def roll(self, betAmmount, betPick):
+    def recordColorHistory(self, theRoll):
+        #   was the roll red?
+        if theRoll in self.red:
+            self.colorHistory.append('Red')
+        #   was the roll black?
+        elif theRoll in self.black:
+            self.colorHistory.append('Black')
+        #   if the roll wasn't black or red, then it was green
+        else:
+            self.colorHistory.append('Green')
+        
+    def rollTheRouletteWheel(self):
+        print('--- Spinning the '+self.rouletteStyle+' roullete wheel ---')
+        #   the picked bet must be an interger between -1 and 36
+        if self.rouletteStyle is 'American':        
+            theRoll = random.randint(-1,36)
+        else:
+            theRoll = random.randint(0,36)
+        #    asign the color history
+        self.recordColorHistory(theRoll)  
+        #    add the roll to the history     
+        self.rollHistory.append(theRoll)
+        print('The ball has landed on '+str(theRoll)+' '+str(self.colorHistory[-1]))
+        return(theRoll)
+        
+        
+    def roll(self, betAmmount, betPick=None):
+        if betPick is None:
+            self.betPick = None
+        else:
+            self.betPick = betPick
         #   Check to see if the bet ammount is valid, if not break
         if float(betAmmount) <= 0.0:
             print('Error: You have bet an incorrect ammount. All bets must be greater than 0.0')
@@ -47,20 +76,14 @@ class roulette:
         #   beggin the Straight roulette bet and game type
         if self.betType is 'Straight':
             if self.rouletteStyle is 'American':
-                print('--- Spinning the '+self.rouletteStyle+' roullete wheel ---')
                 #   the picked bet must be an interger between -1 and 36
                 if int(betPick) < -1 or int(betPick) > 36:
                     print('Error: You have picked an incorrect bit, please roll again with a bet between -1 and 36. American roulette has 38 diferent possible numbers, where 00 is represented as -1')
                     return
                 else:
-                    theRoll = random.randint(-1,36)
-                    
-                    #   asign the color history
-                    self.recordColorHistory(theRoll)
-                    
-                    print('The ball has landed on '+str(theRoll))
+                    theRoll = self.rollTheRouletteWheel()
                     print('You bet the ball would land on '+str(betPick))
-                    self.rollHistory.append(theRoll)
+
                     if theRoll == int(betPick):
                         print('**** !!! You have won !!! ***')
                         print('The payout is 35 to 1')
@@ -76,20 +99,14 @@ class roulette:
                         print('Your new account balance is '+str(self.bankAccount))
                         print('--- End of Roll, Please roll again! ---')
             elif self.rouletteStyle is 'French':
-                print('--- Spinning the '+self.rouletteStyle+' roullete wheel ---')
                 #   the picked bet must be an interger between 0 and 36
                 if int(betPick) < 0 or int(betPick) > 36:
                     print('Error: You have picked an incorrect bit, please roll again with a bet between 0 and 36. French roulette has 37 diferent possible numbers, bets can be from 0 to 36')
                     return
                 else:
-                    theRoll = random.randint(0,36)
-                    
-                    #   asign the color history
-                    self.recordColorHistory(theRoll)
-
-                    print('The ball has landed on '+str(theRoll))
+                    theRoll = self.rollTheRouletteWheel()
                     print('You bet the ball would land on '+str(betPick))
-                    self.rollHistory.append(theRoll)
+
                     if theRoll == int(betPick):
                         print('**** !!! You have won !!! ***')
                         print('The payout is 35 to 1')
@@ -112,17 +129,10 @@ class roulette:
         #   Begin a color choice game
         elif self.betType is 'Color':
             if betPick is 'Red' or betPick is 'Black':
-                print('--- Spinning the '+self.rouletteStyle+' roullete wheel ---')
-                #   the picked bet must be an interger between -1 and 36
-                theRoll = random.randint(-1,36)
-                
-                #   asign the color history
-                self.recordColorHistory(theRoll)
-                
-                
-                print('The ball has landed on '+str(theRoll)+' '+str(self.colorHistory[-1]))
+
+                theRoll = self.rollTheRouletteWheel()
                 print('You bet the ball would land on '+betPick)
-                self.rollHistory.append(theRoll)
+
                 if self.colorHistory[-1] is betPick:
                     print('**** !!! You have won !!! ***')
                     print('The payout is 1 to 1')
@@ -139,27 +149,34 @@ class roulette:
                     print('--- End of Roll, Please roll again! ---')
             else:
                 print('Error: You have picked an incorrect color. Acceptable colors are Red and Black.')
+                return
+        #   Begin a row betting game
+        elif self.betType is 'Row':
+            #   A row bet is a bet on 0 or 00
+            if self.rouletteStyle is 'American':
+                theRoll = self.rollTheRouletteWheel()
+                print('You bet "Row", that ball would land on 0, or 00')
+
+                if theRoll == 0 or theRoll == -1:
+                    print('**** !!! You have won !!! ***')
+                    print('The payout is 17 to 1')
+                    payout = 17.0*float(betAmmount)
+                    self.bankAccount = self.bankAccount + payout
+                    print(str(payout)+' has been added to your account')
+                    print('Your new account balance is '+str(self.bankAccount))
+                    print('*** End of Roll, Please roll again! ***')
+                else:
+                    print('Oh No!, You have lost')
+                    self.bankAccount = self.bankAccount - float(betAmmount)
+                    print('Your bet of '+str(betAmmount)+' has been removed from your account')
+                    print('Your new account balance is '+str(self.bankAccount))
+                    print('--- End of Roll, Please roll again! ---')
+            else:
+                print('Sorry row betting is only allowed on the American roulette wheel. Please set the roullete style to American by roulette.(rouletteStyle="American")')
+                return
         #    You have chosen an incorrect betting game type            
         else:
             print('Error: We have yet to code this type of betting')
             print('The possible betting game types are: Straight, Color')
             print('EX: Set betting game type to Straight with roulette.(betType="Straight")')
 
-    def recordColorHistory(self, theRoll):
-        #   was the roll red?
-        if theRoll in self.red:
-            self.colorHistory.append('Red')
-        #   was the roll black?
-        elif theRoll in self.black:
-            self.colorHistory.append('Black')
-        #   if the roll wasn't black or red, then it was green
-        else:
-            self.colorHistory.append('Green')
-    
-    #def getColor(self, theRoll):
-    #    if theRoll in self.red:
-    #        return('Red')
-    #    if theRoll in self.black:
-    #        return('Black')
-    #    else:
-    #        return('Green')
--- playRoulette.py
+++ playRoulette.py
@@ -8,8 +8,20 @@ play = roulette(rouletteStyle='French',bankAccount=10.0)
 #    play.roll(1,0)
 #    if play.bankAccount == 0.0:
 #        break
+#play.rouletteStyle='American'
+#play.roll(1,-1)
         
-play = roulette(rouletteStyle='American', bankAccount=100.0, betType='Color')
+play = roulette(rouletteStyle='American', bankAccount=150.0, betType='Color')
 
-play.roll(50,'Black')
-        
+#play.roll(50,'Black')
+#        
+#play.betType = 'Straight'
+#play.roll(100, -1)
+#play.roulettyStyle = 'French'
+#play.betType = 'Color'
+#play.roll(1,'Black')
+
+play.betType = 'Row'
+play.roll(5)
+
+#   I need to check if my Row bet type is legit, and if the payout is really 17 to 1
